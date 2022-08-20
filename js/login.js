7b6622a5d0d0b9c377acefe0a11fd03908511d4f
Fibra87 <110511165+Fibra87@users.noreply.github.com>
2022-08-20 20:36:28 -0300

    Edicion Login
    Se edita html del login y se configura .js para que el mismo funcione de acuerdo a validacion.
    Se agregan algunos estilos en css para visualizacion.

---
+++ js/login.js
@@ -0,0 +1,23 @@
+function validar() {
+    let email = document.getElementById("floatingInput").value
+    let password = document.getElementById("floatingPassword").value
+  // inicio booleano bandera
+    let valido = true
+ 
+    if (email == ""){
+        alert("Falta email");
+        valido= false;
+    }
+
+    if (password== ""){
+        alert("Falta password");
+        valido= false;
+    }
+    //si las condiciones son validas redirecciona a pagina inicio.
+    if (valido)
+     return window.location.href = "index.html";
+    }
+
+    //evento iniciador de la funcion.
+
+document.getElementById("login").addEventListener("click", validar)
